fc8712060c7e17e6c5b0b91affbcd9a5c9dd03d6
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-06 19:43:25 +0200

    l5 hw sava2 task1 complete

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Pasisveikinimas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_input = new System.Windows.Forms.Button();
+            this.tb_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_input
+            // 
+            this.bt_input.Location = new System.Drawing.Point(12, 12);
+            this.bt_input.Name = "bt_input";
+            this.bt_input.Size = new System.Drawing.Size(235, 23);
+            this.bt_input.TabIndex = 0;
+            this.bt_input.Text = "PRADETI";
+            this.bt_input.UseVisualStyleBackColor = true;
+            this.bt_input.Click += new System.EventHandler(this.bt_input_Click);
+            // 
+            // tb_output
+            // 
+            this.tb_output.Location = new System.Drawing.Point(12, 41);
+            this.tb_output.Multiline = true;
+            this.tb_output.Name = "tb_output";
+            this.tb_output.Size = new System.Drawing.Size(235, 123);
+            this.tb_output.TabIndex = 1;
+            this.tb_output.TextChanged += new System.EventHandler(this.tb_output_TextChanged);
+            // 
+            // Pasisveikinimas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 184);
+            this.Controls.Add(this.tb_output);
+            this.Controls.Add(this.bt_input);
+            this.Name = "Pasisveikinimas";
+            this.Text = "Pasisveikinimas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_input;
+        private System.Windows.Forms.TextBox tb_output;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Pasisveikinimas : Form
+    {
+        public Pasisveikinimas()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_input_Click(object sender, EventArgs e)
+        {
+            DateTime data = DateTime.Now;
+            tb_output.Text = $"Sveiki. As esu Martynas!";
+            tb_output.AppendText ($"\r\nSiandien yra {data}");
+            
+        }
+    }
+}
