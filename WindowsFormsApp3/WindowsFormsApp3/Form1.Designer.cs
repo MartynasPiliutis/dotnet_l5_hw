9197a32bdc4cc8babff7d3324c8b47f965e9a150
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2020-01-06 20:34:07 +0200

    l5 hw sava2 task3 complete

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_input = new System.Windows.Forms.Button();
+            this.tb_input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt_input
+            // 
+            this.bt_input.Location = new System.Drawing.Point(12, 45);
+            this.bt_input.Name = "bt_input";
+            this.bt_input.Size = new System.Drawing.Size(178, 23);
+            this.bt_input.TabIndex = 0;
+            this.bt_input.Text = "Tikrinti";
+            this.bt_input.UseVisualStyleBackColor = true;
+            this.bt_input.Click += new System.EventHandler(this.bt_input_Click);
+            // 
+            // tb_input
+            // 
+            this.tb_input.Location = new System.Drawing.Point(12, 12);
+            this.tb_input.Name = "tb_input";
+            this.tb_input.Size = new System.Drawing.Size(178, 20);
+            this.tb_input.TabIndex = 1;
+            this.tb_input.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_input.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_input_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 88);
+            this.Controls.Add(this.tb_input);
+            this.Controls.Add(this.bt_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_input;
+        private System.Windows.Forms.TextBox tb_input;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char chr = e.KeyChar;
+            if (!Char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Prasau ivesti skaiciu...");
+            }
+        }
+
+        private void bt_input_Click(object sender, EventArgs e)
+        {
+            double skaicius = Convert.ToDouble(tb_input.Text);
+            
+            if (skaicius % 2 == 0)
+            {
+                MessageBox.Show($"Skaicius {skaicius} yra lyginis");
+            }
+            else
+                MessageBox.Show($"Skaicius {skaicius} yra nelyginis");
+        }
+    }
+}
